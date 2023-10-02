73b5c36443528fae9c45f979baca341848e07f35
Ramy9999 <ramysarraf93@gmail.com>
2023-10-02 15:37:39 -0600

    1 file modified

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,8 +3,6 @@ import '../style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 // import NewTodo from './newTodo';
 
-
-
 function Todo({ title, status }) {
   const [todoVisible, setTodoVisible] = useState(true);
   function completeTodoHandler() {
@@ -13,11 +11,49 @@ function Todo({ title, status }) {
   return (
     <>
       {todoVisible && [
-        <p>
-          {title}
-          <button type="button" className="btn btn-success">
+        <p
+          style={{
+            borderStyle: 'ridge',
+            borderRadius: 5,
+            borderWidth: 1.6,
+            borderColor: 'white',
+            height: 65,
+            position: 'relative',
+          }}
+        >
+          {/* <div */}
+          {/* // style={{ */}
+          {/* //   margin: 0,
+          //   position: 'absolute',
+          //   top: '50%',
+          //   msTransform: 'translateY(-50%)',
+          //   transform: 'translateY(-50%)',
+          // }
+          > */}
+          <div
+            style={{
+              fontSize: 25,
+              msTransform: 'translateY(25%)',
+              transform: 'translateY(25%)',
+              float: 'left',
+              marginLeft: 20,
+            }}
+          >
+            {title}
+          </div>
+          <button
+            style={{
+              msTransform: 'translateY(25%)',
+              transform: 'translateY(25%)',
+              float: 'right',
+              marginRight: 20,
+            }}
+            type="button"
+            className="btn btn-success align-middle"
+          >
             {status}
-          </button>
+          </button>{' '}
+          {/* // </div> */}
         </p>,
       ]}
     </>
